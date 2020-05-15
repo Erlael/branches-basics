ce09fd61476404d5cd22d05b45dccaa3f7829a75
Erlael <kuran001@mail.ru>
2020-05-15 21:11:23 +0700

    Make nice.c

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "H.h"
 
 int main()
 {
+int n;
 printf ("Hello world");
+n=1;
+nice (n);
+n=2;
+nice (n);
+nice (n++);
 return 0;
 }
---
+++ nice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "H.h"
+
+void nice (int n)
+{
+if (n==1)
+printf ("Such a great weather\n");
+if (n==2)
+printf("I hope you sit at home and feel good");
+if (n==3)
+printf ("I was glad to see you. Have a nice day");
+}
