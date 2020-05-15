207a3f412fa8d3d0139178ff9fcce780a0253848
Erlael <kuran001@mail.ru>
2020-05-15 21:17:33 +0700

    Edited nice.c

---
+++ nice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "H.h"
+
+void nice (int n)
+{
+if (n==1)
+printf ("\nSuch a great weather\n");
+if (n==2)
+printf("I hope you sit at home and feel good\n");
+if (n==3)
+printf ("I was glad to see you. Have a nice day\n");
+}
